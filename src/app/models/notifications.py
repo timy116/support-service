7a860d908268644845c52f6f2f222c0697730edf
TimyLian <timy117@gmail.com>
2024-10-01 15:57:59 +0800

    Update notification module fields

--- src/app/models/notifications.py
+++ src/app/models/notifications.py
@@ -1,18 +1,21 @@
 from datetime import datetime, date
+from uuid import UUID
 
 from beanie import Document, Indexed
 
-from app.core.enums import NotificationCategories, NotificationTypes
+from app.core.enums import NotificationCategories, NotificationTypes, LogLevel
 from app.utils.datetime import get_datetime_utc_8
 
 
 class Notification(Document):
     date: Indexed(date)
+    correlation_id: UUID
     category: NotificationCategories
     type: NotificationTypes
+    level: LogLevel
     message: str
     created_at: datetime = get_datetime_utc_8()
 
     class Settings:
         name = "notifications"
-        indexes = ["date", "category", "type"]
+        indexes = ["date", "category", "type", "level"]
