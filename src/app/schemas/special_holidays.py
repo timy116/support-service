a1705f71ae246971a0d652c096b533cc8bf5c71f
TimyLian <timy117@gmail.com>
2024-10-06 20:21:36 +0800

    Update schemas of special holidays

--- src/app/schemas/special_holidays.py
+++ src/app/schemas/special_holidays.py
@@ -1,18 +1,14 @@
 import datetime
-from typing import Optional
 
 from pydantic import BaseModel
 
 from app.models.special_holidays import HolidayInfo
 
 
-class Holiday:
+class Holiday(BaseModel):
     date: datetime.date
     info: HolidayInfo
 
 
-class HolidayCreate(BaseModel):
-    date: datetime.date
-    name: str
-    holiday_category: str
-    description: Optional[str] = None
+class HolidayCreate(Holiday):
+    pass
