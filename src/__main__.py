0e0a91c782bf2a2cce9635c29579b5b9cbfa193d
TimyLian <timy117@gmail.com>
2024-10-07 00:22:30 +0800

    Refactor creating app instance from a `create_app` function instead of getting an app instance directly
    Because it will raise some errors if we get an `app` instance directly when testing endpoints

--- src/__main__.py
+++ src/__main__.py
@@ -6,10 +6,11 @@ from app.core.config import settings
 def run_dev_server() -> None:
     """Run the uvicorn server in development environment."""
     uvicorn.run(
-        "app.main:app",  # path to the ASGI application
+        "app.main:create_app",  # path to the ASGI application
         host=settings.UVICORN_HOST,
         port=settings.UVICORN_PORT,
         reload=settings.DEBUG,
+        factory=True,
     )
 
 
--- src/app/main.py
+++ src/app/main.py
@@ -42,68 +42,66 @@ responses: Set[int] = {
     status.HTTP_500_INTERNAL_SERVER_ERROR,
 }
 
-app = FastAPI(
-    debug=settings.DEBUG,
-    title=settings.PROJECT_NAME,
-    version=settings.PROJECT_VERSION,
-    description="Fast and reliable support service powered by FastAPI and MongoDB.",
-    # Set current documentation specs to v1
-    openapi_url=f"/api/{settings.API_V1_STR}/openapi.json",
-    docs_url=None,
-    redoc_url=None,
-    default_response_class=ORJSONResponse,
-    openapi_tags=tags_metadata,
-    lifespan=lifespan,
-    responses={
-        status.HTTP_422_UNPROCESSABLE_ENTITY: {
-            "description": "Validation Error",
-            "model": APIValidationError,  # Adds OpenAPI schema for 422 errors
+
+def create_app():
+    application = FastAPI(
+        debug=settings.DEBUG,
+        title=settings.PROJECT_NAME,
+        version=settings.PROJECT_VERSION,
+        description="Fast and reliable support service powered by FastAPI and MongoDB.",
+        # Set current documentation specs to v1
+        openapi_url=f"/api/{settings.API_V1_STR}/openapi.json",
+        docs_url=None,
+        redoc_url=None,
+        default_response_class=ORJSONResponse,
+        openapi_tags=tags_metadata,
+        lifespan=lifespan,
+        responses={
+            status.HTTP_422_UNPROCESSABLE_ENTITY: {
+                "description": "Validation Error",
+                "model": APIValidationError,  # Adds OpenAPI schema for 422 errors
+            },
+            **{
+                code: {
+                    "description": HTTPStatus(code).phrase,
+                    "model": CommonHTTPError,
+                }
+                for code in responses
+            },
         },
-        **{
-            code: {
-                "description": HTTPStatus(code).phrase,
-                "model": CommonHTTPError,
-            }
-            for code in responses
-        },
-    },
-)
-
-static_dir = os.environ.get('STATIC_DIR', 'src/app/static')
-app.mount("/static", StaticFiles(directory=static_dir), name="static")
-
-# Add the router responsible for all /api/ endpoint requests
-app.include_router(api.router)
-
-if settings.USE_CORRELATION_ID:
-    from app.middlewares.correlation import CorrelationMiddleware
-
-    app.add_middleware(CorrelationMiddleware)
-
-
-# Custom HTTPException handler
-@app.exception_handler(StarletteHTTPException)
-async def http_exception_handler(_, exc: StarletteHTTPException) -> ORJSONResponse:
-    return ORJSONResponse(
-        content={
-            "message": exc.detail,
-        },
-        status_code=exc.status_code,
-        headers=exc.headers,
     )
+    static_dir = os.environ.get('STATIC_DIR', 'src/app/static')
+    application.mount("/static", StaticFiles(directory=static_dir), name="static")
+    # Add the router responsible for all /api/ endpoint requests
+    application.include_router(api.router)
+    if settings.USE_CORRELATION_ID:
+        from app.middlewares.correlation import CorrelationMiddleware
+
+        application.add_middleware(CorrelationMiddleware)
+
+    # Custom HTTPException handler
+    @application.exception_handler(StarletteHTTPException)
+    async def http_exception_handler(_, exc: StarletteHTTPException) -> ORJSONResponse:
+        return ORJSONResponse(
+            content={
+                "message": exc.detail,
+            },
+            status_code=exc.status_code,
+            headers=exc.headers,
+        )
+
+    @application.exception_handler(RequestValidationError)
+    async def custom_validation_exception_handler(
+            _,
+            exc: RequestValidationError,
+    ) -> ORJSONResponse:
+        return ORJSONResponse(
+            content=APIValidationError.from_pydantic(exc).dict(exclude_none=True),
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+        )
+
+    return application
 
 
-@app.exception_handler(RequestValidationError)
-async def custom_validation_exception_handler(
-        _,
-        exc: RequestValidationError,
-) -> ORJSONResponse:
-    return ORJSONResponse(
-        content=APIValidationError.from_pydantic(exc).dict(exclude_none=True),
-        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
-    )
-
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+if __name__ == "__main__":
+    app = create_app()
